ca7b123a180bfc63326483cbbf9792d10b844a10
Rieke12 <mareke.prahm@outlook.de>
2019-01-03 13:25:29 +0100

    4th commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190102180857) do
+ActiveRecord::Schema.define(version: 20190103114706) do
 
   create_table "cars", force: :cascade do |t|
     t.string   "make"
     t.string   "model"
     t.integer  "year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "lamps", force: :cascade do |t|
+    t.string   "make"
+    t.string   "model"
+    t.integer  "watt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
